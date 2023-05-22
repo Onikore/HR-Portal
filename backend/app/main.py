2be5e823fc213cd3668ac578e1ea7724e76c46f2
Dmitry Grishin <grishinda10@ya.ru>
2023-05-22 19:39:21 +0500

    add CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,20 @@
 import uvicorn
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from app.api.api_v1.api import api_router
 from app.db.session import db
 
 app = FastAPI(title='HR Cab', openapi_url='/api/v1/openapi.json', version='0.0.1')
 app.include_router(api_router)
+
+app.add_middleware(
+        CORSMiddleware,
+        allow_origins=['*'],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
 
 @app.on_event("startup")
